52149e1caa204f1c3f00c7f4da71b63adc253ddb
rjlee <rokjunlee@berkeley.edu>
2018-12-10 13:57:58 -0800

    work

--- tests.R
+++ tests.R
@@ -4,6 +4,207 @@ library(testthat)
 source("Rscript/RJ.R")
 
 
+# ------functions-------------------------
+##########################################
+
+logfunc <- function(func_x){
+  # docstring
+  # simply 'log's the function that you want
+  # func_x is the function that you want to 'log'
+  log_func <- function(x) {}
+  old_f <- rlang::get_expr(body(func_x))
+  body(log_func) <- rlang::get_expr(quo(log(!!old_f)))
+  return(log_func)
+}
+
+numeric_first_d <- function(f, x){
+  # docstring
+  # this function numerically calculates first derivative
+  # f is the underlying function
+  # x is where we want to evaluate
+  # below we are taking limit as h goes to 0
+  h <- .Machine$double.eps^(1/4)
+  numerator <- f(x+h) - f(x)
+  denominator <- h
+  val <- numerator / denominator
+  return(val)
+}
+
+numeric_sec_deri <- function(f, x){
+  # docstring
+  # some functions are hard to derive the second derivative
+  # so this calculates second derivative value numerically
+  # f is the underlying function
+  # x is where we want to evaluate
+  # below we are taking limit as h goes to 0
+  h <- .Machine$double.eps^0.25
+  numerator <- f(x+h) - 2*f(x) + f(x-h)
+  denominator <- h^2
+  val <- numerator / denominator
+  return(val)
+}
+
+c <- integrate(f, min, max)$value #normalizing constant
+g_func <- function(f, x){
+  # docstring:
+  # f is the underlying density function
+  # left_b and right_b are lower and upper bound, respectively
+  # x is where you want to evaluate
+  # after calculating the normalizing constant c  
+  # this function simply creates g as described in the paper
+  g <- f(x) / c
+  return(g)
+}
+
+h <- function(x) {log(g_func(f,x))}
+dh <- function(x) {numeric_first_d(h, x)}  
+
+u_func = function(x, sp){
+  # docstring
+  # function calculates the upper hull piece corresponding to 
+  # the data sampled from Sk
+  # sp is the starting points
+  # x is the sampled point
+  z <- numeric(length(sp) + 1)
+  z[1] <- min
+  z[length(z)] <- max
+  z_middle <- sort(z(sp))
+  z[2:(length(z)-1)] <- z_middle
+  index = findInterval(x, z)
+  u <- h(sp[index]) + (x - sp[index])*dh(sp[index])
+  return(u)
+}
+
+
+l_func <- function(x, sp) {
+  # docstring
+  # function calculates the lower hull piece corresponding to 
+  # the data sampled from Sk, set to -inf if x<x1 or x>xk
+  # sp is the starting points
+  # x is the sampled point
+  index <- findInterval(x, c(min, sp, max))
+  l <- 0
+  
+  if (x <= sp[(length(sp))] & x >= sp[1]){
+    l<- ((sp[index] - x) * h(sp[index - 1]) + (x - sp[index - 1]) * h(sp[index])) / 
+      (sp[index] -sp[index - 1])} 
+  else{
+    l<- -Inf
+  }
+  return(l)
+}
+
+
+z <- function(support){
+  x0 <- head(support, n=-1)
+  x1 <- tail(support, n=-1)
+  zed <- x0 + (h(x0) - h(x1) + (x1 - x0)*dh(x1)) / (dh(x1) - dh(x0))
+  return(zed)	
+}
+
+
+u_k = function(y, support) 
+{
+  u_plus = rep(0, length(y))
+  zed = z(support)
+  
+  piecewise.idx = findInterval(y, c(min, zed, max))
+  npieces = length(zed) + 2
+  for(pidx in 1:npieces){
+    yp = y[piecewise.idx == pidx]
+    xx = h(support[pidx]) + (yp - support[pidx])*dh(support[pidx])
+    u_plus[piecewise.idx == pidx] = xx
+  }
+  return(u_plus)
+}
+
+
+plus.cdf = function(x, sp) {
+  zed = z(sp)
+  
+  zlen = length(zed) ##NUMBER OF CDF POINTS
+  s_k_cdf = numeric(length(x))
+  normaliser = 0
+  for(i in 0:zlen) {
+    if(i == 0)
+    {
+      z_lower = -Inf
+    } else {
+      z_lower = zed[i]
+    }
+    
+    if(i == zlen)
+    {
+      z_upper = Inf
+    } else {
+      z_upper = zed[i+1]
+    }
+    
+    s_k_sample = function(support)
+    {
+      zed = z(support)
+      sp = plus.cdf(zed, support)
+      zpct = sp$required_cdf
+      normaliser = sp$normaliser
+      ub = c(0, zpct, 1)
+      
+      unif.samp = runif(1)
+      
+      fidx = findInterval(unif.samp, ub)
+      num.intervals = length(ub) - 1
+      zlow = c(min, zed)
+      res = rep(0, length(unif.samp))
+      for(i in 1:num.intervals)
+      {
+        ui = unif.samp[ fidx == i ]
+        
+        if(length(ui) == 0)
+        {
+          next
+        }
+        
+        ## Invert the  CDF
+        yp = support[i]
+        zm = zlow[i]
+        tmp = (ui - ub[i]) * dh(yp) * normaliser / exp(h(yp)) + exp( (zm - yp)*dh(yp) )
+        tmp = yp + log(tmp) / dh(yp)
+        res[ fidx == i ] = tmp
+      }
+      return(res)
+  }
+    
+    
+
+    
+
+
+
+##########################################
+# ------functions-------------------------
+
+
+
+# g_func
+
+
+
+
+
+
+test_that("")
+
+g_func <- function(f, x){
+  # docstring:
+  # f is the underlying density function
+  # left_b and right_b are lower and upper bound, respectively
+  # x is where you want to evaluate
+  # after calculating the normalizing constant c  
+  # this function simply creates g as described in the paper
+  g <- f(x) / c
+  return(g)
+}
+
+
 # numeric_first_d
 test_that("function that numerically evaluates first derivative", {
   
